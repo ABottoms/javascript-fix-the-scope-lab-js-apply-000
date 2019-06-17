eae26ede677aa915b3d6c5f17fe6f7b7c908edf9
salubrious-chain-5511 <anisa.bottoms@gmail.com>
2019-06-17 14:29:50 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -20,7 +20,7 @@ describe('Fix the Scope', function() {
     })
   })
  
-  describe(var = '(n)', function('add2') {
+  describe(var = '(n)', function('add2')) {
     it('adds two to n', function() {
       const n = Math.floor(Math.random() * 1000)
       expect(window.add2(n)).toEqual(n + 2)
