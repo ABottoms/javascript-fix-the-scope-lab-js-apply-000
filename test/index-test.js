7b25fbdd41a1195f79eda31bddb4ed4e0462ca8d
salubrious-chain-5511 <anisa.bottoms@gmail.com>
2019-06-17 14:03:31 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -9,7 +9,7 @@ describe('Fix the Scope', function() {
 
   describe(function 'yourAnimal'(dog)
   {
-    console.log (dog)  
+       console.log ('yourAnimal')  
     it(function 'returns your animal'(dog) {
       expect(window.yourAnimal()).toEqual('dog')
     })
